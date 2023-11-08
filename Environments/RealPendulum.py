39ef9051da463a592fe524e856a3dec776321f58
PeriniM <perinim.98@gmail.com>
2023-11-08 15:01:22 +0100

    refactor code

--- Environments/RealPendulum.py
+++ Environments/RealPendulum.py
@@ -21,27 +21,28 @@ class RealPendulum:
         self.name = "RealPendulum"
         self.nbJoint = 1
         self.num_state = 2
+        self.action = 0.0
         self.state = np.zeros(self.num_state)
-        self.iterCount = 0
+        self.motorAngle = 0.0
         self.done = False
-        self.homing = False
+        self.iterCount = 0
         self.omega_max = 10.0
 
-    def normalize_state(self, state):
-
-        normalized_angle = state[0] / np.pi  # Normalizes to [-1, 1]
-        normalized_velocity = state[1] / self.omega_max  # Normalizes to [-1, 1]
-        return np.array([normalized_angle, normalized_velocity])
+        # variable to store angles of one episode
+        self.episode_angles = []
     
     def reset(self):
         """
         Reset the environment to the initial state.
         """
+        # Reset the episode angles
+        self.episode_angles = []
+
         # Send command to pendulum to go to home position.
         self.send_serial("0,1")
         # Wait for the pendulum to report it has finished resetting.
         while (1):
-            self.state, self.done = self.reader.get_state()
+            self.state, self.motorAngle, self.done = self.reader.get_state()
             if not self.done:
                 break
 
@@ -49,36 +50,24 @@ class RealPendulum:
         self.iterCount = 0
         normalized_state = self.normalize_state(self.state)
         return normalized_state
-
+    
     def step(self, action):
         """
         Take a step in the environment.
         """
-        while (1):
-            self.state, self.done = self.reader.get_state()
-            if self.done:
-                self.homing = True
-            else:
-                break
+        # Send action to pendulum over serial
+        self.send_serial(f"{action},0")
+        # Read state and episode done flag from serial
+        self.state, self.motorAngle, self.done = self.reader.get_state()
 
-        # Send action to pendulum if not in homing process.
-        if not self.homing:
-            self.send_serial(f"{action},0")
-            # Read state and episode done flag from serial
-            self.state, self.done = self.reader.get_state()
-
-        # Calculate reward only when not homing.
-        reward = self.calculate_reward(self.state)
-        self.iterCount += 1
+        # Store the angles of the episode for reward penalty
+        self.episode_angles.append(self.state[0])
         
-        # Check for end of episode based on iteration count.
-        if self.iterCount >= self.maxIter:
-            self.done = True
-        if self.homing:
-            self.done = True
-            self.homing = False
-        
+        # Calculate reward
         normalized_state = self.normalize_state(self.state)
+        reward = self.calculate_reward(normalized_state)
+        self.iterCount += 1
+        self.reset_policy(self.maxIter)
 
         return normalized_state, reward, self.done
 
@@ -88,28 +77,54 @@ class RealPendulum:
         """
         self.ser.write(f"{command}\n".encode())
         time.sleep(0.1)
+    
+    def normalize_state(self, state):
+        """
+        Normalize the state to [-1, 1].
+        """
+        normalized_angle = state[0] / np.pi  # Normalizes to [-1, 1]
+        normalized_velocity = state[1] / self.omega_max  # Normalizes to [-1, 1]
 
-    def calculate_reward(self, state):
-
+        return np.array([normalized_angle, normalized_velocity])
+    
+    def reset_policy(self, reset_count=200):
+        """
+        Reset the system when a condition is met.
+        """
+        if self.iterCount > reset_count:
+            self.done = True
+    
+    def calculate_reward(self, normalized_state):
+        """
+        Calculate the reward for the current state.
+        """
         # Constants to scale the angle and velocity penalties
         ANGLE_WEIGHT = 1.0
-        VELOCITY_WEIGHT = 0.1  # Lower value since we want to give less weight to velocity
-        ANGLE_TARGET = 0.0  # Upright position
+        VELOCITY_WEIGHT = 0.1
+        MOTOR_ANGLE_WEIGHT = 0.1
+        ACTION_WEIGHT = 0.01
 
-        # Penalize the deviation of the angle from the upright position
-        angle_deviation = np.abs(state[0] - ANGLE_TARGET)
+        # Penalize the angle to be minimized
+        angle_penalty = ANGLE_WEIGHT * (normalized_state[0] ** 2)
+        # Penalize the angular velocity to be minimized
+        velocity_penalty = VELOCITY_WEIGHT * (normalized_state[1] ** 2)
 
-        # The cosine of the angle can be used for a bounded reward, peaking at the upright position.
-        # angle_penalty = ANGLE_WEIGHT * (1 - np.cos(angle))
+        # Penalize the motor angle to be minimized
+        normalized_motor_angle = self.motorAngle / 180.0
+        motor_angle_penalty = MOTOR_ANGLE_WEIGHT * (normalized_motor_angle ** 2)
 
-        # Alternatively, a quadratic penalty is often used in control tasks
-        angle_penalty = ANGLE_WEIGHT * (angle_deviation ** 2)
-
-        # Penalize the angular velocity to prefer solutions that don't require much movement
-        velocity_penalty = VELOCITY_WEIGHT * (state[1] ** 2)
+        # Penalize the action to be minimized
+        normalized_action = self.action / np.max(self.range_actions)
+        action_penalty = ACTION_WEIGHT * (normalized_action ** 2)
 
         # Reward is higher when penalties are lower
-        reward = -(angle_penalty + velocity_penalty)
+        reward = -(angle_penalty + velocity_penalty + motor_angle_penalty + action_penalty)
+
+        # Penalize the reward if the average angle of the episode is close to pi
+        # after 3/4 of the maximum iterations
+        if self.iterCount > self.maxIter*3/4:
+            if np.abs(np.mean(self.episode_angles)) < (np.pi-0.8):
+                reward-=100.0
 
         return reward
 
--- Environments/SerialReader.py
+++ Environments/SerialReader.py
@@ -8,6 +8,7 @@ class SerialReader(threading.Thread):
         self.ser = ser
         self.daemon = True  # Set thread as daemon so it will end with the main program
         self.state = np.zeros(2)
+        self.motorAngle = 0.0
         self.episode_done = False
         self.lock = threading.Lock()  # Lock to prevent simultaneous read/write of state
 
@@ -16,13 +17,14 @@ class SerialReader(threading.Thread):
             if self.ser.in_waiting:
                 line = self.ser.readline().decode().strip()
                 parts = line.split(',')
-                if len(parts) == 3:
+                if len(parts) == 4:
                     with self.lock:
                         self.state = np.array([float(parts[0]), float(parts[1])])
-                        self.episode_done = bool(float(parts[2]))
+                        self.motorAngle = float(parts[2])
+                        self.episode_done = bool(float(parts[3]))
                         # print(self.state, self.episode_done)
 
     def get_state(self):
         time.sleep(0.1)
         with self.lock:
-            return self.state.copy(), self.episode_done
+            return self.state.copy(), self.motorAngle, self.episode_done
